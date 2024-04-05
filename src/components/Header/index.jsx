41c4b3dd6107b516efb7e2f34831648c379c4364
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-04-04 21:09:16 -0300

    feat: created header with theme switcher

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import iconMoon from "../../assets/images/icon-moon.svg";
+import iconSun from "../../assets/images/icon-sun.svg";
+import { useContext } from "react";
+import { AppContext } from "../../appContext";
+import "./header.scss";
+
+export default function Header() {
+  const { theme, setTheme } = useContext(AppContext);
+
+  function toggleTheme() {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  }
+
+  return (
+    <header className={theme}>
+      <div className="header-content">
+        <span className="title">TODO</span>
+        <button className="btn-toggle-theme" onClick={toggleTheme}>
+          <img src={theme === "light" ? iconMoon : iconSun} alt="Icon" />
+        </button>
+      </div>
+    </header>
+  );
+}
