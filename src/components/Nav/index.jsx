9410996749de06bf52dffbb508bae9a02852f14b
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-04-10 20:13:36 -0300

    feat: implemented fn to show tasks that have not yet been completed

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,18 +1,53 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../../appContext";
 
 import "./nav.scss";
 
 export default function Nav() {
-  const { theme } = useContext(AppContext);
+  const { theme, tasks, setTasks, tasksFiltered, setTasksFiltered } =
+    useContext(AppContext);
+  const [itemsLeft, setItemsLeft] = useState(0);
+
+  function handleFilteredTasks(filter) {
+    setTasksFiltered(filter);
+  }
+
+  useEffect(() => {
+    function getItemsLeft() {
+      if (tasks.length === 0) {
+        setItemsLeft(0);
+        return;
+      }
+
+      const incompleteTasks = tasks.filter((item) => item.completed === false);
+      setItemsLeft(incompleteTasks.length);
+    }
+
+    getItemsLeft();
+  }, [tasks, setTasks]);
 
   return (
     <nav className={theme}>
-      <span>5 items left</span>
+      <span>{`${itemsLeft} items left`} </span>
       <div className="navigation">
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+        <button
+          className={tasksFiltered === "all" ? "active" : ""}
+          onClick={() => handleFilteredTasks("all")}
+        >
+          All
+        </button>
+        <button
+          className={tasksFiltered === "active" ? "active" : ""}
+          onClick={() => handleFilteredTasks("active")}
+        >
+          Active
+        </button>
+        <button
+          className={tasksFiltered === "completed" ? "active" : ""}
+          onClick={() => handleFilteredTasks("completed")}
+        >
+          Completed
+        </button>
       </div>
       <button className="btn-clear">Clear completed</button>
     </nav>
